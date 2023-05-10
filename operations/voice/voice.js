a531bd33bc68a8e8f0d0182b70145983b23ff999
Lupin <santimw18@gmail.com>
2023-05-09 23:24:12 -0500

    Se agregaron los archivos correspondientes a la función de sonido tanto de reconocimiento de voz como de respuesta por parte del sistema (versión 1.1.0)

--- operations/voice/voice.js
+++ operations/voice/voice.js
@@ -1,20 +1,51 @@
-const btnStart = document.getElementById('voice');
-const text_area = document.getElementById('text');
+var artyom = new Artyom();
 
-const rec = new webkitSpeechRecognition();
-
-rec.continuous = true;
-rec.lang = 'es-ES';
-rec.interimResult = false;
-
-btnStart.addEventListener('click', ()=>{
-    rec.start();
+document.querySelector("#voz").addEventListener('click', function(){
+    artyom.say("sonido activado");
 });
 
-rec.onresult = (event) =>{
-    const txt = event.results[event.results.length - 1][0].transcript;
-    console.log(txt);
-    text_area.value = txt;
-    console.log(txt.event);
-}
+artyom.addCommands({
+    indexes: [
+        "ABRIR PUERTA PRINCIPAL",
+        "CERRAR PUERTA PRINCIPAL", 
+        "ABRIR VENTANA DE SALA",
+        "CERRAR VENTANA DE SALA"
+    ],
+    action: function(i){
+        if(i == 0){      
+            document.querySelector("#mainDoor").style.transform = "rotate(20deg)";
+            artyom.say("ABRIENDO PUERTA PRINCIPAL");
+            console.log("Puerta Principal abierta");
+        }else if(i == 1){
+            artyom.say("CERRANDO PUERTA PRINCIPAL");
+            document.querySelector("#mainDoor").style.transform = "rotate(0deg)"
+            console.log("Puerta Principal abierta");
+        }else if(i == 2) {
+            artyom.say("ABRIENDO VENTANA DE SALA");
+            document.querySelector("#windowLivingRoom").style.backgroundColor = "white";
+            console.log("Ventana de sala abierta");
+        }else if(i == 3) {
+            artyom.say("CERRANDO VENTANA DE SALA");
+            document.querySelector("#windowLivingRoom").style.backgroundColor = "rgb(143, 225, 255)";
+            console.log("Ventana de sala cerrada");
+        }
+    }
+});
+
+artyom.initialize({
+    lang:"es-ES",
+    debug:true,
+    listen:true,
+    continuous: true,
+    speed:0.9,
+    mode:"normal"
+});
+
+artyom.redirectRecognizedTextOutput(function(recognized,isFinal){
+    if(isFinal){
+        console.log("Texto final reconocido: " + recognized);
+    }else{
+        console.log(recognized);
+    }
+});
 
